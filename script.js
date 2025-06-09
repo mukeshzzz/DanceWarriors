78fc5afb49f4711b6bbcd15c7e1610221ed99aee
Mukesh <160158616+mukeshzzz@users.noreply.github.com>
2025-06-09 13:03:56 +0530

    files commited

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  // tsParticles.loadJSON("tsparticles", "particles.json").then(() => {
+  //   console.log("tsParticles loaded");
+  // }).catch(err => {
+  //   console.error("tsParticles error:", err);
+  // });
