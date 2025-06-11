6cdb997f09d3ef98046b294968b48c6b98aa33d8
Mukesh <160158616+mukeshzzz@users.noreply.github.com>
2025-06-11 20:32:52 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-  // tsParticles.loadJSON("tsparticles", "particles.json").then(() => {
-  //   console.log("tsParticles loaded");
-  // }).catch(err => {
-  //   console.error("tsParticles error:", err);
-  // });
