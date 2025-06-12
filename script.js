d924d5741f666f76b1d0c6ca06c8aeb08cc0138e
mukeshzzz <mukeshselvamani380@gmail.com>
2025-06-12 12:10:57 +0530

    commited script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('toggle');
+  navLinks.classList.toggle('active');
+});
