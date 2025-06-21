8f52a888e0abaabff59d3ed86a0d4da0278bc404
mukeshzzz <mukeshselvamani380@gmail.com>
2025-06-21 22:33:45 +0530

    files modified

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    const hamburger = document.getElementById('hamburger');
+    const nav = document.querySelector('.nav-links');
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+  });
+});
+
+
   const videos = document.querySelectorAll("video");
 
   videos.forEach(video => {
